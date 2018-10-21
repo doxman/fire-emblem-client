392d8fbc0b8485345e6b5ebbbd45885e10d0488c
Daniel Oxman <zeoxyman@gmail.com>
2018-10-21 12:45:30 -0400

    Now fetching map image from server instead of holding it locally

--- src/FireEmblemClient.js
+++ src/FireEmblemClient.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import map from './map.png';
 import './FireEmblemClient.css';
 
 class FireEmblemClient extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mapImageSrc: null,
+        };
+    }
+
+    componentDidMount() {
+        const headers = new Headers();
+        headers.append("Content-Type", "image/png");
+        const mapRequest = new Request("http://localhost:8888/map", {
+            headers,
+        });
+        fetch(mapRequest).then((response) => response.blob()).then((blob) => {
+            const mapImageSrc = URL.createObjectURL(blob);
+            this.setState({
+                mapImageSrc,
+            });
+        });
+    }
+
     render() {
+        const {mapImageSrc} = this.state;
+
         return (
             <div className="FireEmblemClient">
                 <h1 className="FireEmblemClient-title">Fire Emblem</h1>
                 <div className="FireEmblemClient-map">
-                    <img src={map} className="FireEmblemClient-map-image" alt="map"/>
+                    {mapImageSrc == null ? null :
+                        <img src={mapImageSrc} className="FireEmblemClient-map-image" alt="map"/>
+                    }
                 </div>
             </div>
         );
